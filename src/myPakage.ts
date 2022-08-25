bdd9a973c228a22ef3b4da6b6c03d514cd19753d
CCCWS <eung0626@gmail.com>
2022-08-26 00:38:24 +0900

    typescript

--- src/myPakage.ts
+++ src/myPakage.ts
@@ -6,3 +6,17 @@ init({
 });
 
 exit(1);
+
+type Last = <G>(arr: G[]) => G;
+const last: Last = (arr) => {
+  return arr[arr.length - 1];
+};
+
+type Prepend = <G>(arr: G[], item: G) => G[];
+const prepend: Prepend = (arr, item) => {
+  arr.unshift(item);
+  return arr;
+};
+
+console.log(last<number>([1, 2, 3, 4]));
+console.log(prepend([2, 3, 4], 1));
